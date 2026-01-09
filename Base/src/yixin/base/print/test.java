f8249ebfdf0a8fb718db63e06d4fdc224f85ed86
yixin <yixincode@163.com>
2026-01-09 13:39:44 +0800

    print test

---
+++ Base/src/yixin/base/print/test.java
@@ -0,0 +1,21 @@
+package yixin.base.print;
+
+public class test {
+    public static void main(String [] args) {
+        System.out.println("\t\t\t图书清单");
+        System.out.print("书名\t\t\t");
+        System.out.print("数量\t\t");
+        System.out.print("单价\t\t");
+        System.out.println("金额");
+
+        System.out.print("平凡的世界\t");
+        System.out.print("2\t\t");
+        System.out.print("50\t\t");
+        System.out.println("100");
+
+        System.out.print("阿泰勒角落\t");
+        System.out.print("2\t\t");
+        System.out.print("50\t\t");
+        System.out.println("90 ");
+    }
+}
