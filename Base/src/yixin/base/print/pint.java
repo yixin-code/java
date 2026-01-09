e8094cd587bffb4aacbff3b2da9ad88dc7ae16c1
yixin <yixincode@163.com>
2026-01-09 13:04:58 +0800

    print

--- Base/src/yixin/base/print/pint.java
+++ Base/src/yixin/base/print/pint.java
@@ -1,4 +1,8 @@
 package yixin.base.print;
 
-public class pint {
+public class pint{
+    public static void main(String[] args) {
+        System.out.print("hello yixin "); // 不换行
+        System.out.println("hello java"); // 换行
+    }
 }
