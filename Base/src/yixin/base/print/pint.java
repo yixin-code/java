4fdfa0781c365732d2bc0a05e41666a25aa2f88f
yixin <yixincode@163.com>
2026-01-20 13:04:39 +0800

    shortcut key

--- Base/src/yixin/base/print/pint.java
+++ Base/src/yixin/base/print/pint.java
@@ -1,7 +1,9 @@
 package yixin.base.print;
 
 public class pint{
+    // psvm main函数
     public static void main(String[] args) {
+        // sout System.out.println
         System.out.print("hello yixin "); // 不换行 同样支持 \n \t
         System.out.println("hello java"); // 换行
     }
